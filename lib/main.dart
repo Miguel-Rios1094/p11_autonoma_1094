e486493ee562394a55cb046128c0d5b8f5ff9ec9
a.22308051281094 <a.22308051281094@cbtis128.edu.mx>
2025-03-03 19:03:26 +0000

    aaa

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,91 @@
-import 'package:flutter/material.dart';
-import 'dart:ui';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const InnerShadowEffect(),
-    );
-  }
-}
-
-class InnerShadowEffect extends StatelessWidget {
-  const InnerShadowEffect({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title:Text('Miguel Rios Mat:22308051281094', style: TextStyle(color: const Color.fromARGB(255, 0, 0, 0), fontWeight: FontWeight.bold)),
-          backgroundColor: const Color.fromARGB(255, 125, 245, 69),
-          centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            innerShadowContainer(200, 80, BorderRadius.circular(20)),
-            const SizedBox(height: 40),
-            innerShadowContainer(200, 80, BorderRadius.circular(40)),
-            const SizedBox(height: 40),
-            innerShadowContainer(80, 80, BorderRadius.circular(100)),
-          ],
-        ),
-      ),
-      backgroundColor: Colors.grey[200],
-    );
-  }
-
-  Widget innerShadowContainer(double width, double height, BorderRadius borderRadius) {
-    return Container(
-      width: width,
-      height: height,
-      decoration: BoxDecoration(
-        borderRadius: borderRadius,
-        color: const Color.fromARGB(255, 3, 148, 8),
-      ),
-      child: Stack(
-        children: [
-          Positioned.fill(
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: borderRadius,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.2),
-                    blurRadius: 10,
-                    offset: const Offset(5, 5),
-                    spreadRadius: -5,
-                  ),
-                  BoxShadow(
-                    color: const Color.fromARGB(255, 112, 248, 124).withOpacity(0.8),
-                    blurRadius: 10,
-                    offset: const Offset(-5, -5),
-                    spreadRadius: -5,
-                  ),
-                ],
-              ),
-            ),
-          ),
-          const Center(
-            child: Text(
-              'Figura',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+import 'package:flutter/material.dart'; 
+  
+void main() { 
+  runApp(RunMyApp()); 
+} 
+  
+class RunMyApp extends StatelessWidget { 
+  const RunMyApp({super.key}); 
+  
+  @override 
+  Widget build(BuildContext context) { 
+    return MaterialApp( 
+      theme: ThemeData(primarySwatch: Colors.green), 
+      debugShowCheckedModeBanner: false, 
+      home: Scaffold( 
+        appBar: AppBar( 
+          title:Text('Miguel Rios Mat:22308051281094', style: TextStyle(color: const Color.fromARGB(255, 252, 252, 252), fontWeight: FontWeight.bold)),
+          backgroundColor: const Color.fromARGB(255, 189, 19, 13),
+          centerTitle: true, 
+        ), 
+        body: Center( 
+          child: Column( 
+              
+            children: [ 
+              SizedBox(height: 50,), 
+              Container( 
+                child: Center(child: Text('Geeks for Geeks')), 
+                height: 100, 
+                width: 250, 
+                decoration: BoxDecoration( 
+                  boxShadow: [ 
+                    BoxShadow( 
+                      color: Colors.green, 
+                    ), 
+                    BoxShadow( 
+                      color: Colors.white70, 
+                      spreadRadius: -5.0, 
+                      blurRadius: 20.0, 
+                    ), 
+                  ], 
+                ), 
+              ), 
+              SizedBox( 
+                height: 50, 
+              ), 
+              Container( 
+                child: Center(child: Text('Geeks for Geeks')), 
+                height: 100, 
+                width: 250, 
+                decoration: BoxDecoration( 
+                  borderRadius: BorderRadius.circular(20), 
+                  boxShadow: [ 
+                    BoxShadow( 
+                      color: Colors.green, 
+                    ), 
+                    BoxShadow( 
+                      color: Colors.white70, 
+                      spreadRadius: -5.0, 
+                      blurRadius: 20.0, 
+                    ), 
+                  ], 
+                ), 
+              ), 
+              SizedBox( 
+                height: 50, 
+              ), 
+              Container( 
+                child: Center(child: Text('Geeks for Geeks')), 
+                height: 100, 
+                width: 100, 
+                decoration: BoxDecoration( 
+                  borderRadius:BorderRadius.circular(50) , 
+                  boxShadow: [ 
+                    BoxShadow( 
+                      color: Colors.green, 
+                    ), 
+                    BoxShadow( 
+                      color: Colors.white70, 
+                      spreadRadius: -5.0, 
+                      blurRadius: 20.0, 
+                    ), 
+                  ], 
+                ), 
+              ), 
+            ], 
+          ), 
+        ), 
+      ), 
+    ); 
+  } 
+} 
